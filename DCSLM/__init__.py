7501461e2ebdf25e44cb1981231727b575818979
Pearce Bergh <pearcebergh@gmail.com>
2024-04-28 10:39:49 -0700

    version increment

--- DCSLM/__init__.py
+++ DCSLM/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 __license__ = 'BSD-3'
-__copyright__ = '2023, Pearce Bergh <pearce@pearce.cool>'
+__copyright__ = '2024, Pearce Bergh <pearce@pearce.cool>'
 __docformat__ = 'restructuredtext en'
